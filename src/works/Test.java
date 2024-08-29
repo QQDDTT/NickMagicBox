70b64b4e2df39532e15effa431de9fda2d08f30f
QQDDTT <hunengwei0414@gmail.com>
2024-08-29 15:34:55 +0900

    2024/08/29

---
+++ src/works/Test.java
@@ -0,0 +1,23 @@
+package works;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int a = 1;
+        /* 
+        byte
+        short
+        int
+        long
+        float
+        double 
+        */
+
+        //char
+        //String
+
+        //boolean
+
+        System.out.println(a);
+    }
+}
