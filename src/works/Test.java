abae01fa4aba25470aca2e58f93d6198e5b70666
QQDDTT <hunengwei0414@gmail.com>
2024-09-05 15:15:28 +0900

    2024/09/25

--- src/works/Test.java
+++ src/works/Test.java
@@ -1,23 +1,24 @@
 package works;
 
+import static  works.demo.demo01;
 public class Test {
 
     public static void main(String[] args) {
-        int a = 1;
-        /* 
-        byte
-        short
-        int
-        long
-        float
-        double 
-        */
+        String t = text("hello world")
+                    .concat(" ")
+                    .concat("a")
+                    .concat(" ")
+                    .replace("l","X")
+                    .substring(0, 2)
+                    .toUpperCase()
+                    .toLowerCase()
+                    .split("").toString();
+        System.out.println(t);
+        demo01();
+    }
 
-        //char
-        //String
-
-        //boolean
-
-        System.out.println(a);
+    private static String text(String a) {
+        System.err.println("text length : " + a.length());
+        return a;
     }
 }
---
+++ src/works/demo.java
@@ -0,0 +1,7 @@
+package works;
+
+public class demo {
+    public static void demo01(){
+        System.out.println("demo 01");
+    }
+}
