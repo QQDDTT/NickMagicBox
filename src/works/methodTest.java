ee79e402740bc7adc06e93c07aad54dc44dfab2c
QQDDTT <hunengwei0414@gmail.com>
2024-09-12 16:21:12 +0900

    2024/09/12

---
+++ src/works/methodTest.java
@@ -0,0 +1,12 @@
+package works;
+
+public class methodTest {
+
+    public static String demo1(int i,int j){
+        return "" + i + j;
+    }
+
+    public static String demo2(int i,int j){
+        return i + j + "";
+    }
+}
